52f581aa840d3a42f14d6b1e0e99915a62db0a53
Glasc <63518997+Glasc@users.noreply.github.com>
2024-02-13 13:11:14 -0600

    Initial setup

---
+++ src/data/site-config.ts
@@ -0,0 +1,10 @@
+const siteConfig = {
+  hero: {
+    backgroundImage: {
+      src: "/background.avif",
+    },
+  },
+  carousel: {},
+};
+
+export default siteConfig;
---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-100": "#DAF0FB",
+        "primary-200": "#98C8F0",
+        "primary-300": "#1B539D",
+        "primary-400": "#081D4F",
+        "primary-500": "#01051E",
+        "secondary-100": "#F7B6A6",
+        "secondary-200": "#DC3728",
+        "secondary-300": "#750800",
+        "gray-100": "#F1F4F9",
+        "gray-200": "#D1D9E6",
+        "gray-300": "#97A4BA",
+        "gray-400": "#67748E",
+        "gray-500": "#4C5467",
+        "gray-600": "#363C49",
+      },
+    },
+  },
+  plugins: [],
+};
