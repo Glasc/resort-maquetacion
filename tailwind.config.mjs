e5613dbf7b2bc9948bd74f6d546817d6468d3558
Glasc <63518997+Glasc@users.noreply.github.com>
2024-02-21 15:27:50 -0600

    Reorder imports and update paths in Astro and Tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,6 @@
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
@@ -24,5 +23,4 @@ export default {
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
 };
