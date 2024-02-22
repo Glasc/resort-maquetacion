34ba200697f2d5e04205779b6a1c2a162200f93c
Glasc <63518997+Glasc@users.noreply.github.com>
2024-02-22 12:30:18 -0600

    Remove import of pagination CSS in carousel.ts

--- src/scripts/carousel.ts
+++ src/scripts/carousel.ts
@@ -2,7 +2,6 @@ import Swiper from "swiper";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
 
 new Swiper(".spot-showcase-carousel", {
   modules: [Navigation, Pagination, Autoplay],
